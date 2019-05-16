d0978324d074365135055827baf8d49c66eb5926
Noah Matsuyoshi <nmats@uw.edu>
2019-05-15 20:25:46 -0700

    Get percentiles for all tests for both ages

--- OkNoAhH.py
+++ OkNoAhH.py
@@ -5,30 +5,51 @@ import os
 
 # Inputs (Assume inputs are valid at this point, will handle validation in web form)
 GMFCSLevel = "III"
-age = [3, 5]
+age = [[3, 5], [4, 3]] # [[past year, past month], [current year, current month]]
+
+# Assign scores in this order: "ECAB", "SMWT", "SAROMM", "CEDLpar", "CEDLsc", "EASE", "FSA", "HEALTH", "GMFM"
+# With first element being past score and second current
+scores = [[25, 30], [25, 30], [25, 30], [25, 30], [25, 30], [25, 30], [25, 30], [25, 30], [25, 30]]
 
 def getRowNumFromAge(age):
     monthRow = round(age[1]/3)
     return age[0]*4+monthRow-6
 
+def findStartAndEndCol(cells):
+    startAndEnd = []
+    for index in range(len(cells)):
+        if(cells[index].ctype == 2 and len(startAndEnd) == 0):
+            startAndEnd.append(index)
+        elif(cells[index].ctype != 2 and len(startAndEnd) == 1):
+            startAndEnd.append(index-1)
+            return startAndEnd
+
 
 file_name = "data.xlsx"
-#testNames = ["ECAB", "SMWT", "SAROMM", "CEDLpar", "CEDLsc", "EASE", "FSA", "HEALTH", "GMFM"]
-testNames = ["ECAB"] #For testing run only ECAB
-percentileStartCol = 2
-percentileEndColMax = 22 #For longest row
+testNames = ["ECAB", "SMWT", "SAROMM", "CEDLpar", "CEDLsc", "EASE", "FSA", "HEALTH", "GMFM"]
+#testNames = ["ECAB"] #For testing run only ECAB
+percentileRow = 1
 
+numTests = len(testNames)
 workbook = xlrd.open_workbook(file_name)
-row = getRowNumFromAge(age)
-for name in testNames:
-    sheetName = name+"_Level_"+GMFCSLevel
+percentiles = numpy.zeros((numTests, 2))
+
+for testIndex in range(numTests):
+    sheetName = testNames[testIndex]+"_Level_"+GMFCSLevel
     sheet = workbook.sheet_by_name(sheetName)
-    percentileEndCol = percentileEndColMax
-    if(sheetName=="ECAB_Level_I"):
-        percentileEndCol = 13
-    elif(sheetName=="ECAB_Level_II"):
-        percentileEndCol = 18
-    selectedRow = sheet.row_slice(row, percentileStartCol, percentileEndCol)
-    print(selectedRow[0].value)
+    
+    for timeIndex in range(2):
+        row = getRowNumFromAge(age[timeIndex])
+        selectedRow = sheet.row(row)
+        
+        [percentileStartCol, percentileEndCol] = findStartAndEndCol(selectedRow)
+        selectedRow = sheet.row_slice(row, percentileStartCol, percentileEndCol)
+        #closestScore = min(selectedRow, key = lambda x:abs(x.value - ECABScore[0]))
+        closestScoreIndex = 0
+        for cellIndex in range(len(selectedRow)):
+            
+            if(float(selectedRow[cellIndex].value) <= scores[testIndex][timeIndex]):
+                closestScoreIndex = cellIndex
+        percentiles[testIndex][timeIndex] = float(sheet.cell(percentileRow, closestScoreIndex+percentileStartCol).value)
 
-
+print(percentiles)
