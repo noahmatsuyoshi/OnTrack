31f3cde6ebbe6098b403e3c832a11d52ec3d110b
drbone2357 <sierrabonilla14@gmail.com>
2019-05-23 18:02:55 -0700

    website with tabs

--- OnTrackWebsite/views.py
+++ OnTrackWebsite/views.py
@@ -3,16 +3,16 @@ from django.http import HttpResponse
 #handling traffic on home page
 #loading template
 def home(request):
-    return render(request, 'OnTrackWebsite/home.html')
+    return render(request, 'OnTrackWebsite/home.html', {'title': 'Home'})
 
 def acknowledgements(request):
-    return render(request, 'OnTrackWebsite/acknowledgements.html')
+    return render(request, 'OnTrackWebsite/acknowledgements.html', {'title': ': Acknowledgements'})
 
 def how_to_use_output(request):
-    return render(request, 'OnTrackWebsite/how_to_use_output.html')
+    return render(request, 'OnTrackWebsite/how_to_use_output.html', {'title': ': how to use the output information'})
 
 def how_it_calculates(request):
-    return render(request, 'OnTrackWebsite/how_it_calculates.html')
+    return render(request, 'OnTrackWebsite/how_it_calculates.html', {'title': ': how it calculates'})
 
 def checkin(request):
-    return render(request, 'OnTrackWebsite/checkin.html')
+    return render(request, 'OnTrackWebsite/checkin.html', {'title': 'Check in!'})
